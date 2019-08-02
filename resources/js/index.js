d427fa26b6e4940d4e54271bbb40c47e51565222
SecretPunk <amanjit.gill1@gmail.com>
2019-08-02 15:19:57 +1000

    Add pageload transition and toggle up/down arrows.

--- resources/js/index.js
+++ resources/js/index.js
@@ -7,9 +7,19 @@
 var contentHeadings = document.querySelectorAll(".content-heading");
 var numContentHeadings = contentHeadings.length;
 
+window.onload = function() {
+
+  document.querySelector("body").classList.add("into-position");
+};
+
 for (var i = 0; i < numContentHeadings; i++) {
   contentHeadings[i].addEventListener("click", function() {
 
     this.parentNode.querySelector(".content-body").classList.toggle("full-height");
+
+    var arrowIcon = this.querySelector("span");
+
+    arrowIcon.classList.toggle("fa-angle-down");
+    arrowIcon.classList.toggle("fa-angle-up");
   });
 }
