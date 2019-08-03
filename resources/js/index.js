09253bbb685b48323c640f4e437ccb82bb70f655
SecretPunk <amanjit.gill1@gmail.com>
2019-08-03 15:18:34 +1000

    Begin implementing Guardian API call.

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,6 +1,7 @@
 /* jshint strict: global */
 /* jshint devel: true */
 /* jshint browser: true */
+/* jshint esversion: 6 */
 
 'use strict';
 
@@ -10,6 +11,17 @@ var numContentHeadings = contentHeadings.length;
 window.onload = function() {
 
   document.querySelector("body").classList.add("into-position");
+
+  var request = new XMLHttpRequest();
+  request.open('GET', 'http://content.guardianapis.com/sections?q=business&api-key=ca502747-cd8a-4290-9c72-ccd2e8db9c2a', true);
+  request.send();
+
+  request.onload = function() {
+    var data = JSON.parse(this.response);
+    console.log(data);
+    document.querySelector(".content-body").innerHTML = data.response.status;
+  };
+
 };
 
 for (var i = 0; i < numContentHeadings; i++) {
