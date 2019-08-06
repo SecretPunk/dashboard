38d9a63acd0f98f4f6120fb14ea4fcd628a2d155
SecretPunk <amanjit.gill1@gmail.com>
2019-08-06 20:09:04 +1000

    Access weather data; yet unformatted.

--- resources/js/index.js
+++ resources/js/index.js
@@ -13,7 +13,15 @@ const G_ORDER_BY = "order-by=newest";
 const G_NUM_RESULTS = 10;
 const G_PAGE_SIZE = "page-size=" + G_NUM_RESULTS;
 const G_API_KEY = "api-key=ca502747-cd8a-4290-9c72-ccd2e8db9c2a";
-const G_URL = G_ENDPOINT + "&" + G_SEARCH_TERM + "&" + G_SECTION + "&" + G_ORDER_BY + "&" + G_PAGE_SIZE + "&" + G_API_KEY;
+const G_URL = G_ENDPOINT + G_SEARCH_TERM + "&" + G_SECTION + "&" + G_ORDER_BY + "&" + G_PAGE_SIZE + "&" + G_API_KEY;
+
+// OpenWeatherMap API
+const W_ENDPOINT = "https://api.openweathermap.org/data/2.5/forecast?";
+const W_CITY_ID = "id=2155718";
+const W_API_KEY = "appid=649156a9240a1ebacc8e6395876a40ea";
+const W_UNITS = "units=metric"; // to output temperatures in Celsius (default is Kelvin)
+const W_URL = W_ENDPOINT + W_CITY_ID + "&" + W_API_KEY + "&" + W_UNITS;
+const W_NUM_RESULTS = 8; // api gives 5-day, 3-hourly, forecasts (40 in total). Just use the first 8 (covers 24 hours).
 
 window.onload = function() {
 
@@ -22,6 +30,8 @@ window.onload = function() {
   setToggles(); // set the content headings to toggle the content on/off
 
   getPolitics(); // get politics news from Guardian API and insert into politics section
+
+  getWeather(); // get weather data from OpenWeatherMap API and insert into weather section
 };
 
 function setToggles() {
@@ -52,11 +62,36 @@ function getPolitics() {
     var parsedResponse = JSON.parse(this.response);
 
     var contentBody = document.querySelector(".politics .content-body");
+
     for (var i = 0; i < G_NUM_RESULTS; i++) {
       var newAnchor = document.createElement("a");
       newAnchor.setAttribute("href", parsedResponse.response.results[i].webUrl);
       newAnchor.innerHTML = parsedResponse.response.results[i].webTitle;
       contentBody.appendChild(newAnchor);
+    }
+  };
+}
+
+function getWeather() {
+
+  var request = new XMLHttpRequest();
+  request.open('GET', W_URL, true);
+  request.send();
+
+  request.onload = function() {
+    var parsedResponse = JSON.parse(this.response);
+    console.log(parsedResponse);
+
+    var contentBody = document.querySelector(".weather .content-body");
+
+    for (var i = 0; i < W_NUM_RESULTS; i++) {
+      var newForecast = document.createElement("p");
+      console.log(parsedResponse.list);
+      var icon = parsedResponse.list[i].weather[0].icon + ".png";
+      console.log(icon);
+      var temp = parsedResponse.list[i].main.temp;
+      newForecast.innerHTML = parsedResponse.list[i].weather[0].description + " " + temp;
+      contentBody.appendChild(newForecast);
     }
   };
 }
